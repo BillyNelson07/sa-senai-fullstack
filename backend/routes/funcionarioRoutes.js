8744e8747ac05761f8807edac0d4ecbaa31c72b5
GABRIEL EDUARDO <gabriel_eduardo150@estudante.sesisenai.org.br>
2026-08-17 21:17:45 -0300

    feat: update funcionario routes and implement Dashboard component with CRUD functionality

--- backend/routes/funcionarioRoutes.js
+++ backend/routes/funcionarioRoutes.js
@@ -3,7 +3,7 @@ import FuncionarioController from "../controllers/funcionarioController.js";
 
 const router = express.Router();
 
-router.post("/post", FuncionarioController.criarFuncionario);
+router.post("/", FuncionarioController.criarFuncionario);
 
 // Exporta o router para ser usado no index.js
 export default router;
