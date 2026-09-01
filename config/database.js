c25b0439536b5a20d575618ac9f7804bf959e1ff
GABRIEL EDUARDO <gabriel_eduardo150@estudante.sesisenai.org.br>
2026-09-01 19:52:46 -0300

    feat: update database configuration to use DATABASE_URL and clean up environment variables

--- config/database.js
+++ config/database.js
@@ -3,17 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Configurando a instância do Sequelize
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
+// Configurando a instância do Sequelize com a URL única do Render
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Necessário para aceitar o certificado SSL do Render
+    },
   },
-);
+});
 
 export default sequelize;
